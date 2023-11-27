3f3fa45f333380ff46da180f54ef47a2c2493477
Dcool <90443691+LSW2717@users.noreply.github.com>
2023-11-28 02:25:54 +0900

    .

--- database/mogu_web.sql
+++ database/mogu_web.sql
@@ -14,8 +14,8 @@ create table contest
 );
 
 INSERT INTO contest (name, category, end_date, img_url, url) VALUES
-('실감미디어 콘텐츠 제작 공모전', 'inha', '2024-01-15', './asset/img/img1.png', 'https://open.kakao.com/o/g864AJUf'),
-('ZEPETO WORLD JAM FALL 2023', 'game', '2023-12-11', './asset/img/img2.png', 'https://open.kakao.com/o/gdbFCJUf'),
-('인공지능윤리 아이디어 경진대회', 'AI', '2023-12-3', './asset/img/img3.png', 'https://open.kakao.com/o/gElkHJUf'),
-('K-PaaS 아이디어 공모전', 'idea', '2023-12-13', './asset/img/img4.png', 'https://open.kakao.com/o/glSB1HUf'),
-('엔지니어링산업 대국민 콘텐츠 공모전', 'contest', '2024-01-31', './asset/img/img5.png', 'https://open.kakao.com/o/gFrQNJUf');
+('실감미디어 콘텐츠 제작 공모전', 'inha', '2024-01-15', './assets/asset/img/img1.png', 'https://open.kakao.com/o/g864AJUf'),
+('ZEPETO WORLD JAM FALL 2023', 'game', '2023-12-11', './assets/asset/img/img2.png', 'https://open.kakao.com/o/gdbFCJUf'),
+('인공지능윤리 아이디어 경진대회', 'AI', '2023-12-3', './assets/asset/img/img3.png', 'https://open.kakao.com/o/gElkHJUf'),
+('K-PaaS 아이디어 공모전', 'idea', '2023-12-13', './assets/asset/img/img4.png', 'https://open.kakao.com/o/glSB1HUf'),
+('엔지니어링산업 대국민 콘텐츠 공모전', 'contest', '2024-01-31', './assets/asset/img/img5.png', 'https://open.kakao.com/o/gFrQNJUf');
