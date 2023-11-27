d76c0711fe5a813b12074f64856df7073e092b7e
김혁래 <hrkim0101@naver.com>
2023-11-27 17:44:32 +0900

    프론트

--- front/mogufront/src/App.js
+++ front/mogufront/src/App.js
@@ -1,4 +1,4 @@
-import MyImage from './images/image8.jpg';
+import MyImage from './images/building.jpg';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,10 +19,11 @@ function App() {
               <p>프로젝트를 함께 진행할 <span className="highlight-yellow">개발자</span> / <span className="highlight-red">디자이너</span>를
               <br></br><b>모구</b>와 함께 모집하고 구해보세요!</p>
               <Link to="/kakao" className="link-class"><span><b>시작하기 {'>'}</b></span></Link>
-              <div class="fixed-footer">
+              
+            </div>
+            <div class="fixed-footer">
                 <div>참여 정보는 저희 프로젝트를 발전시키기 위한 자료로<br></br>쓰일 수 있음을 알려드립니다.</div>
               </div>
-            </div>
           </div> 
         } /> 
         <Route path="/kakao" element={<Kakao />} />
